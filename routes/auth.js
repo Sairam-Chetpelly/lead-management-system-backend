5ba5f1085e8ff00188b1361d335921a94cba7b0f
Sairam-Chetpelly <sairamchettpelli@gmail.com>
2025-09-02 16:57:19 +0530

    email logs

--- routes/auth.js
+++ routes/auth.js
@@ -127,6 +127,8 @@ router.post('/forgot-password', [
     // Send email
     const transporter = nodemailer.createTransport({
       service: 'gmail',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
