704753c404fac5632b9f394f2fa60524849507e0
Sairam-Chetpelly <sairamchettpelli@gmail.com>
2025-09-11 13:56:51 +0530

    lead gen fail emails

--- routes/leads.js
+++ routes/leads.js
@@ -24,6 +24,8 @@ const { refreshMetaToken, getCurrentToken } = require('../utils/metaTokenRefresh
 // Email configuration
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
